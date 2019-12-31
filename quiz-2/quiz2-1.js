5f944e0acf4c6b579fcdb2e01921cd1181baa1b4
jakyoungkim <jakyoung.kim@catenoid.net>
2019-12-31 11:04:15 +0900

    2번 문제

--- quiz-2/quiz2-1.js
+++ quiz-2/quiz2-1.js
@@ -1,5 +1,41 @@
-class quiz2 {
-    constructor () {
-        console.log('quiz2');
+function solution(p) {
+    if (p.length % 2 !== 0) {
+        alert('짝수가 아닙니다.');
+        return;
     }
+    var total_value = 0;
+    var answer = '';
+    var start_index = 0;
+    var end_index = 0;
+    var check = true;
+    for (var i=0; i<p.length; i++) {
+        var index_text = p.substr(i, 1);
+        var check_value;
+        if (index_text === '(') {
+            check_value = 1;
+        } else {
+            check_value = -1;
+        }
+        total_value += check_value;
+        if (total_value === 0) {
+            if (!check) {
+                var add_text;
+                check = true;
+                end_index = i;
+                var length = end_index - start_index + 1;
+                if (p.substr(start_index, 1) === ')') {
+                    add_text = p.substr(length/2, length/2) + p.substr(start_index, length/2);
+                } else {
+                    add_text = p.substr(start_index, length);
+                }
+                answer += add_text;
+            }
+        }else {
+            if (check) {
+                check = false;
+                start_index = i;
+            }
+        }
+    }
+    return answer;
 }
